834e8c6d7c0d82d89e5272870935e7c08a1b46ef
ismael <ismael_ariel@hotmail.com>
2014-02-14 18:08:38 -0600

    archivos para el proyecto del chat de html5

---
+++ chat.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var dust = require("dustjs-linkedin");
+var cons = require("consolidate");
+
+var app = express();
+app.listen(8021);
+
+//---- configuracion de carpetas estaticas ----
+app.use("/css", express.static(__dirname + "/css"));
+app.use("/javascript", express.static(__dirname + "/javascript"));
+
+// ----- CONFIGURACION DEL SISTEMA DE TEMPLATES -----
+app.engine("dust", cons.dust);
+app.set("views", __dirname + "/vistas");
+app.set("view engine", "dust");
+
+console.log("servidor web listo");
