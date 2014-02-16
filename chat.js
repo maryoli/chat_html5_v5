084402bd66024706758c0c60661351c0ae10d3a2
Maryoli de los Ángeles <maryoli_ang@hotmail.com>
2014-02-15 18:59:28 -0600

    chat actualizado

--- chat.js
+++ chat.js
@@ -1,9 +1,12 @@
 var express = require("express");
 var dust = require("dustjs-linkedin");
 var cons = require("consolidate");
+var http = require("http"); //LIBRERIA INTERNA
+var socketio = require("socket.io");
 
 var app = express();
-app.listen(8021);
+var servidor = http.createServer(app);
+servidor.listen(8021);
 
 //---- configuracion de carpetas estaticas ----
 app.use("/css", express.static(__dirname + "/css"));
@@ -18,4 +21,20 @@ console.log("servidor web listo");
 
 app.get("/",function(req, res){
 	res.render("cliente");
+});
+
+//habilitamos el web socket en el servidor
+var io = socketio.listen(servidor);
+
+
+//la variable socket representa al usuario que se conectó
+io.sockets.on("connection", function(socket){
+	//el on significa cuando ocurra un evento
+	//el primer argumento es el nombre del evento
+	//el segundo elemento es al función que se ejecuta al producirse ese evento
+	socket.on("mensaje_al_servidor", function(datosCliente){
+		//io.sockets=todos los usuarios conectados
+		//emit produce un evento que puede escuchar el cliente o el servidor
+		io.sockets.emit("mensaje_al_cliente", datosCliente);
+	});
 });
